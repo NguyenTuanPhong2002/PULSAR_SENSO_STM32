d90e1b9fc45d79aa160bca433f5bb82b9e9215ad
Nguyen Tuan Phong <85359726+NguyenTuanPhong2002@users.noreply.github.com>
2024-01-25 10:20:23 +0700

    change the file main from C to C++

--- CODE/Core/Src/main.cpp
+++ CODE/Core/Src/main.cpp
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "interface.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/

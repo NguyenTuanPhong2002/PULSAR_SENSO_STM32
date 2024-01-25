432ba642ebff6e83cc4da40c273a83538d436fcd
Nguyen Tuan Phong <85359726+NguyenTuanPhong2002@users.noreply.github.com>
2024-01-25 09:32:12 +0700

    Create class "logData"

--- CODE/Interface/logData.h
+++ CODE/Interface/logData.h
@@ -9,5 +9,33 @@
 #define LOGDATA_H_
 
 
+class logData
+{
+private:
+    /* data */
+    logData *instancePtr;
+
+    logData(/* args */);
+    ~logData();
+
+public:
+    logData *getInstance(){
+        if(instancePtr == nullptr){
+            instancePtr = new logData;
+        }
+        return instancePtr;
+    }
+
+};
+
+logData::logData(/* args */)
+{
+}
+
+logData::~logData()
+{
+}
+
+
 
 #endif /* LOGDATA_H_ */

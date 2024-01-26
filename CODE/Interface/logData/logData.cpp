9f7e03a113cb7e9a96872ede458967f38e47aab4
Nguyen Tuan Phong <85359726+NguyenTuanPhong2002@users.noreply.github.com>
2024-01-26 10:21:09 +0700

    add lib and creat the instancePtr

--- CODE/Interface/logData/logData.cpp
+++ CODE/Interface/logData/logData.cpp
@@ -4,14 +4,16 @@
  *  Created on: Jan 25, 2024
  *      Author: ACER
  */
-
+#include <cstring> // Include the header for strlen
+#include <stdio.h>
+#include <cstdarg> // Include the necessary header for va_list, va_start, va_end
 
 #include "logData.h"
 
+logData *logData::instancePtr = nullptr;
 
 logData::logData(/* args */)
 {
-
 }
 
 logData::~logData()

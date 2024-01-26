f88f6462e2f5b3c97317edcaec4bfbbf12ae51b6
Nguyen Tuan Phong <85359726+NguyenTuanPhong2002@users.noreply.github.com>
2024-01-26 10:24:02 +0700

    Create pulsar.cpp

---
+++ CODE/Interface/pulsar/pulsar.cpp
@@ -0,0 +1,18 @@
+/*
+ * pulsar.cpp
+ *
+ *  Created on: Jan 26, 2024
+ *      Author: ACER
+ */
+
+#include "pulsar.h"
+
+pulsar *pulsar::instancePtr = nullptr;
+
+pulsar::pulsar(/* args */)
+{
+}
+
+pulsar::~pulsar()
+{
+}

b57fa819c6bb690cb5dc9ccebb44c32bc8e14add
Nguyen Tuan Phong <85359726+NguyenTuanPhong2002@users.noreply.github.com>
2024-01-26 17:30:41 +0700

    create on power and off power of mod bus

--- CODE/Interface/pulsar/pulsar.cpp
+++ CODE/Interface/pulsar/pulsar.cpp
@@ -16,3 +16,13 @@ pulsar::pulsar(/* args */)
 pulsar::~pulsar()
 {
 }
+
+
+void pulsar::modbusOnPower(){
+	HAL_GPIO_WritePin(config.powerPin.Port, config.powerPin.pin, GPIO_PIN_SET);
+}
+
+void pulsar::modbusOffPower(){
+	HAL_GPIO_WritePin(config.powerPin.Port, config.powerPin.pin, GPIO_PIN_RESET);
+}
+
